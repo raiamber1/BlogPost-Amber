fa51dfb8a7b4b6f4fab8ef60702f4630e997c384
amber-rai <raiamber2@gmail.com>
2016-02-11 02:41:05 +0530

    Initial commit

---
+++ app/welcome/offcanvas.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('[data-toggle="offcanvas"]').click(function () {
+        $('.row-offcanvas').toggleClass('active')
+    });
+});/**
+ * Created by rai.amber on 11/23/2015.
+ */
